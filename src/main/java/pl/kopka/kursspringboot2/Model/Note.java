958dc06c23339649566b66df5ab77a148ad82f83
„JakubKopka” <jakubkopka@int.pl>
2020-11-03 21:29:32 +0100

    - init

---
+++ src/main/java/pl/kopka/kursspringboot2/Model/Note.java
@@ -0,0 +1,25 @@
+package pl.kopka.kursspringboot2.Model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private String date;
+}
